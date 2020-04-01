d36d4636f1eda2d9a9455dec1016a056f7c5d945
olgahwang <khvanolga.v@gmail.com>
2020-04-01 01:11:19 -0700

    Speed

--- src/05_Jet.js
+++ src/05_Jet.js
@@ -1,4 +1,4 @@
-var s = 200, t = 0;
+var s = 300, t = 0;
 var s2 = 500, t2 = 0;
 let cnv;
 let fnt;
@@ -18,7 +18,7 @@ function setup() {
 
 function draw() {
   background(0);
-  translate(innerWidth/2, innerHeight);
+  translate(innerHeight/2, innerHeight/2);
   for (i = 0; i < s; i+=20){
     for (j = 0; j <TAU; j +=PI/45) {
       let p;
@@ -26,30 +26,22 @@ function draw() {
         p = txt;
         fill(0, 0, 80);
         R = i/s*TAU*2;
-        textSize(R*1.2);
-        text(p, sin(j+sin(R+j*2)+t)*i*4-400,cos(j-t+R*5)*i*3-450);
+        textSize(R);
+        text(p, sin(j+sin(R+j*2)+t)*i*4-100,cos(j-t+R*5)*i*3);
       } else {
         let z = frameCount % 66;
         if (frameCount % 66 == 0) { col1 = 259;}
         fill(col1, 100, 100);
         p = txt2;
-        R = i/s*TAU*2;
+        R = i*0.5/s*TAU*3;
         textSize(R*2);
-        text(p, sin(j+sin(R+j*2)+t2)*i*1.5-400,cos(j-t2+R*5)*i*1.5-450);
+        text(p, sin(j+sin(R+j)+t2*2)*i-100,cos(j-t2+R*9)*i);
       }
     }
   }
 
-  // for (i = 0; i < s2; i+=100){
-  //   for (j = 0; j <TAU; j +=PI/120) {
-  //     R = i/s2*TAU*noise(i/j, 1, 2);
-  //     fill(72, 84, 123);
-  //     textSize(R*4);
-  //     text(txt2, sin(j+sin(R+j*3)+t2)*i*3,cos(j-t2+R*1.3)*i*5-400);
-  //   }
-  // }
-  t+=0.014;
-  t2+=0.01;
+  t+=0.001;
+  t2+=0.004;
   if (frameCount % 66 < 33 ) {
     col1++;
   } else {
